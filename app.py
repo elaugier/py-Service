938b294170a8c08b8a733a83f27080ff359ee1dc
Eric Laugier <ela@egen.guru>
2018-08-04 14:43:03 +0200

    add cors

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
 # app.py
+import asyncio
+import time
+import base64
 from aiohttp import web
+import aiohttp_cors
 
 async def ping(request):
-    return web.Response(text="pong")
+    data = {
+        'dta':'pong'
+    }
+    return web.json_response(data=data)
+    
 
 app = web.Application()
+
+
 app.add_routes([web.get('/ping', ping)])
+
+# Configure CORS on all routes.
+cors = aiohttp_cors.setup(app,defaults={
+    "*": aiohttp_cors.ResourceOptions(
+            allow_credentials=True,
+            expose_headers="*",
+            allow_headers="*",
+        )
+})
+for route in list(app.router.routes()):
+    cors.add(route)
 
 web.run_app(app)
 
